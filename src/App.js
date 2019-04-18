a827cb6df478306ac6acf90b518c4bb2c8e647f8
rajju <vajrachara@yahoo.com>
2019-04-18 17:40:25 +0100

    gh-pages installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    articles: [],
+    pages: [],
+    blog: [],
+    webformMsg: []
+  }
+
+
+  componentDidMount(){
+//get article
+    axios.get(`https://www.yellow-website.com/d8-JSONAPI/jsonapi/node/article`)
+    .then( res => {
+      this.setState({articles: res.data})
+    })
+    .catch(err => {
+      console.log(err);
+    })
+//get page
+    axios.get(`https://www.yellow-website.com/d8-JSONAPI/jsonapi/node/page`)
+    .then( res => {
+      this.setState({pages: res.data})
+    })
+    .catch(err => {
+      console.log(err);
+    })
+//get blog
+    axios.get(`https://www.yellow-website.com/d8-JSONAPI/jsonapi/node/blog`)
+    .then( res => {
+      this.setState({blog: res.data})
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+
+
+  }
+  
+
+
+
   render() {
+    console.log('article', this.state.articles);
+    console.log('page', this.state.pages);
+    console.log('blog', this.state.blog);
+    
+
+    
+  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h3>Drupal 8 JSON API</h3>
+        
       </div>
     );
   }
