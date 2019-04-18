d44206d5d6b74444de3b698df69eca4882e1c2b4
rajju <vajrachara@yahoo.com>
2019-04-18 17:41:13 +0100

    Updates

---
+++ precache-manifest.2ce4f0c76cdb09d935a6064c9e2e7201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-d8-jsonapi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d905e68e735f153659d4",
+    "url": "/react-d8-jsonapi/static/js/runtime~main.46e3245c.js"
+  },
+  {
+    "revision": "659a50737b3337e00612",
+    "url": "/react-d8-jsonapi/static/js/main.2243437a.chunk.js"
+  },
+  {
+    "revision": "7df5f14d91836dff24b8",
+    "url": "/react-d8-jsonapi/static/js/2.63bf9db2.chunk.js"
+  },
+  {
+    "revision": "659a50737b3337e00612",
+    "url": "/react-d8-jsonapi/static/css/main.650f6630.chunk.css"
+  },
+  {
+    "revision": "5ad806daf6ab245f50a9ea71d9fb58a6",
+    "url": "/react-d8-jsonapi/index.html"
+  }
+];
